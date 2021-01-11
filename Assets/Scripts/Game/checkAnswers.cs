7f287ef936abfeafe974ffc4cb7565570e8ad76b
gustavox13 <gurodrigues1300@gmail.com>
2021-01-11 10:17:38 -0300

    add full assets

---
+++ Assets/Scripts/Game/checkAnswers.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkAnswers : MonoBehaviour
+{
+
+
+    public bool completeChallenge = false;
+    private int numWord = 0;
+
+    [SerializeField]
+    private GameObject gamecontroller;
+
+    [SerializeField]
+    private AudioSource pointAudio;
+
+    [SerializeField]
+    private AudioSource completeAudio;
+
+    //TAGS CORRETAS
+    [SerializeField]
+    private string firstword;
+    [SerializeField]
+    private string secondWord;
+
+
+
+    //OBJ DA PERGUNTA ATUAL
+    [SerializeField]
+    private GameObject emptySlot1;
+    [SerializeField]
+    private GameObject fullSlot1;
+
+    [SerializeField]
+    private GameObject emptySlot2;
+    [SerializeField]
+    private GameObject fullSlot2;
+
+    [SerializeField]
+    private float timeAudio;
+
+    private bool audioFinish = false;
+
+
+
+    private void Start()
+    {
+        StartCoroutine(ListenAudio());
+    }
+
+
+
+
+    IEnumerator ListenAudio()
+    {
+        yield return new WaitForSeconds(timeAudio);
+
+        audioFinish = true;
+
+        AudioFinishAndRhymeComplete();
+    }
+
+
+    private void AudioFinishAndRhymeComplete()
+    {
+        if(numWord == 2 && audioFinish == true)
+        {
+            StartCoroutine(ListencompleteAudio());
+        }
+    }
+
+    IEnumerator ListencompleteAudio()
+    {
+        completeAudio.Play();
+        //REPRODUZ O AUDIO COMPLETO AQUI
+
+        yield return new WaitForSeconds(timeAudio + 2.7f); //TEMPO DE REPRODUZIR O AUDIO COMPLETO
+
+        gamecontroller.GetComponent<GameControler>().RhymeComplete(); //LEVA ATE O SCRIPT GAMECONTROLER E FINALIZA O LVL
+    }
+    
+
+    public void checkCurrentWord(string currentWord)
+    {
+        numWord++;
+
+        if(currentWord == firstword)
+        {
+            emptySlot1.SetActive(false);
+            fullSlot1.SetActive(true);
+
+
+
+        } else if(currentWord == secondWord)
+        {
+            emptySlot2.SetActive(false);
+            fullSlot2.SetActive(true);
+        }
+
+
+         if (numWord == 2)
+         {
+            pointAudio.Play();
+         }
+
+        AudioFinishAndRhymeComplete();
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/GameControler.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] lvls = new GameObject[3];
+
+    [SerializeField]
+    private GameObject pacoca;
+
+    private Animator pacocaAnim;
+
+    [SerializeField]
+    private GameObject tutorial;
+
+    
+
+    [SerializeField]
+    private GameObject finalScreen;
+
+    private int currentLvl = 0;
+
+    [SerializeField]
+    private GameObject audioTuto;
+
+
+    public float timmer;
+
+
+    private bool startGame = false;
+
+    private void Awake()
+    {
+        ShuffleQuestions();
+    }
+
+    private void Start()
+    {
+        pacocaAnim = pacoca.GetComponent<Animator>();
+
+    }
+
+
+    private void Update()
+    {
+        if (startGame == true)
+        {
+            timmer += Time.deltaTime;
+        }
+
+    }
+
+    public void CloseTutoAndStart() //FECHA TUTORIAL E INICIA GAME
+    {
+        tutorial.SetActive(false);
+        StartCoroutine(AudioTutorial());
+    }
+
+    IEnumerator AudioTutorial()
+    {
+        audioTuto.SetActive(true);
+
+        yield return new WaitForSeconds(31f);
+        audioTuto.SetActive(false);
+
+        StartTurn();
+    }
+
+    private void StartTurn() //INICIA TURNO
+    {
+        startGame = true;
+
+        lvls[currentLvl].SetActive(true);
+
+    }
+
+
+    public void RhymeComplete()
+    {
+
+        lvls[currentLvl].SetActive(false);
+
+        if (currentLvl >= 2)
+        {
+            startGame = false;
+
+            finalScreen.SetActive(true);
+        }
+        else
+        {
+            currentLvl++;
+            lvls[currentLvl].SetActive(true);
+        }
+    }
+
+
+    private void ShuffleQuestions()     //EMBARALHAR FRASES
+    {
+        for (int i = 0; i < lvls.Length; i++)
+        {
+            GameObject obj = lvls[i];
+            int randomizeArray = Random.Range(0, i);
+            lvls[i] = lvls[randomizeArray];
+            lvls[randomizeArray] = obj;
+        }
+    }
+
+
+
+
+}
